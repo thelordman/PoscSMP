d19ba65ad9505826154e8987b353aef16b5cbc8d
Hunter-creator <101205562+thelordman@users.noreply.github.com>
2022-09-12 21:46:44 +0300

    Cleanup

--- src/main/java/thelordman/poscsmp/PoscSMP.java
+++ src/main/java/thelordman/poscsmp/PoscSMP.java
@@ -3,15 +3,4 @@ package thelordman.poscsmp;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PoscSMP extends JavaPlugin {
-
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
-
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
 }
